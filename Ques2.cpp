a802b1b177b3dd4d0369c8e6d1645657b576d6e2
Gurpreet Singh <gurpreetsingh4174@gmail.com>
2025-07-19 17:41:47 +0530

    addef a file

---
+++ Ques2.cpp
@@ -0,0 +1,21 @@
+// 1234
+// 1234
+// 1234
+// 1234 WAP to print this Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number of Rows: ";
+    cin>>n;
+    for (int  i = 0; i < 4; i++)
+    {
+       for (int  j = 0; j <n; j++)
+       {
+       cout<<i<<" ";
+       }
+       cout<<endl;
+       
+    }
+    
+}
